b5f62781d4ba5fa5d69b0ccca34dae15a39f1d9c
marjamis <marjamis@amazon.com>
2017-08-31 13:12:11 +1000

    Bare bones golang version working with X-Ray successfully.

--- go/main.go
+++ go/main.go
@@ -1,55 +1,59 @@
 package main
 
 import (
-	"context"
-	"net/http"
+  "context"
+  "net/http"
 
-	"github.com/aws/aws-xray-sdk-go/xray"
+  "github.com/aws/aws-xray-sdk-go/xray"
 
-	// Importing the plugins enables collection of AWS resource information at runtime
-	// _ "github.com/aws/aws-xray-sdk-go/plugins/ec2"
-	// _ "github.com/aws/aws-xray-sdk-go/plugins/beanstalk"
-	// _ "github.com/aws/aws-xray-sdk-go/plugins/ecs"
+  // Importing the plugins enables collection of AWS resource information at runtime
+  // _ "github.com/aws/aws-xray-sdk-go/plugins/ec2"
+  // _ "github.com/aws/aws-xray-sdk-go/plugins/beanstalk"
+  // _ "github.com/aws/aws-xray-sdk-go/plugins/ecs"
 
-	log "github.com/cihub/seelog"
-	"github.com/emicklei/go-restful"
-	_ "net/http/pprof"
-)
-
-var (
-	//CONTEXT is context
-	CONTEXT context.Context
+  log "github.com/cihub/seelog"
+  "github.com/emicklei/go-restful"
+  _ "net/http/pprof"
 )
 
 func init() {
-	xray.Configure(xray.Config{
-		DaemonAddr:     "xray:2000", // default
-		LogLevel:       "info",      // default
-		ServiceVersion: "1.2.3",
-	})
-
-	CONTEXT = context.Background()
+  xray.Configure(xray.Config{
+    DaemonAddr:     "xray:2000", // default
+    LogLevel:       "trace",     // default
+    ServiceVersion: "1.2.3",
+  })
 }
 
 func main() {
-	go func() {
-		log.Info(http.ListenAndServe(":6060", nil))
-	}()
+  go func() {
+    log.Info(http.ListenAndServe(":6060", nil))
+  }()
 
-	ws := new(restful.WebService)
-	ws.Path("/").Consumes(restful.MIME_JSON).Produces(restful.MIME_JSON)
-	ws.Route(ws.GET("/true").To(trueFunction))
-	restful.Add(ws)
+  ws := new(restful.WebService)
+  ws.Path("/").Consumes(restful.MIME_JSON).Produces(restful.MIME_JSON)
+  ws.Route(ws.GET("/true").To(trueFunction))
+  ws.Route(ws.GET("/false").To(falseFunction))
+  restful.Add(ws)
 
-	xray.Capture(CONTEXT, "Internals", func(ctx1 context.Context) (err error) {
-		log.Info("Test")
-		xray.AddMetadata(ctx1, "ResourceResult", "Test")
-		return
-	})
-
-	log.Info("Failed webserver", http.ListenAndServe(":8080", nil))
+  log.Info("Failed webserver", http.ListenAndServe(":8080", nil))
 }
 
 func trueFunction(request *restful.Request, response *restful.Response) {
-	response.WriteEntity("Here")
+  ctx := context.Background()
+  ctx, seg := xray.BeginSegment(ctx, "GoApp")
+  defer seg.Close(nil)
+  xray.Capture(ctx, "trueFunction", func(ctx1 context.Context) (err error) {
+    response.WriteEntity("Here")
+    return
+  })
+}
+
+func falseFunction(request *restful.Request, response *restful.Response) {
+  ctx := context.Background()
+  ctx, seg := xray.BeginSegment(ctx, "GoApp")
+  defer seg.Close(nil)
+  xray.Capture(ctx, "falseFunction", func(ctx1 context.Context) (err error) {
+    response.WriteEntity("NotHere")
+    return
+  })
 }
